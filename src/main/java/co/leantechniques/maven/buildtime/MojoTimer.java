4ef660705f92f33edfe617e5f1e8dc879299d016
Igor Nikolaev <igor.nikolaev@nikisoft.ru>
2017-05-29 00:15:13 +0300

    Output execution times in the order of executions

--- src/main/java/co/leantechniques/maven/buildtime/MojoTimer.java
+++ src/main/java/co/leantechniques/maven/buildtime/MojoTimer.java
@@ -1,6 +1,8 @@
 package co.leantechniques.maven.buildtime;
 
-public class MojoTimer {
+import java.lang.Comparable;
+
+public class MojoTimer implements Comparable<MojoTimer> {
 
     private final String projectName;
 
@@ -47,5 +49,20 @@ public class MojoTimer {
 
     public void accept(TimerVisitor visitor){
         visitor.visit(this);
+    }
+
+    public int compareTo(MojoTimer that) {
+        if (that == null)
+            return 1;
+
+        if (this == that)
+            return 0;
+
+        if (this.startTime > that.startTime)
+            return 1;
+        else if (this.startTime < that.startTime)
+            return -1;
+
+        return 0;
     }
 }
